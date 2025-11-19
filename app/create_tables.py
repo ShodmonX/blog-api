f8d189e5eee395af8d7003410fbfb18a729d4942
ShodmonX <xolmurodovshodmon@gmail.com>
2025-11-19 20:36:44 +0500

    Alembic integrated

--- app/create_tables.py
+++
@@ -1,9 +0,0 @@
-from .db.base import Base, engine
-from .models.user import User
-from .models.post import Post
-
-
-async def init_models():
-    async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
