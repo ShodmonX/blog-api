100fe5d85fd1dcbb2388669c0a3f2ac6471199d8
ShodmonX <xolmurodovshodmon@gmail.com>
2025-11-19 20:49:31 +0500

    Alembic tested succesfully

--- app/models/user.py
+++ app/models/user.py
@@ -11,5 +11,7 @@ class User(Base):
     username: Mapped[str] =  mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
+    bio: Mapped[str] = mapped_column(String(255), nullable=True)
+    profile_pic: Mapped[str] = mapped_column(String(255), nullable=True)
 
     posts = relationship("Post", back_populates="owner", cascade="all, delete-orphan")
--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 import uvicorn
 import asyncio
-from app.create_tables import init_models
 
 if __name__ == "__main__":
-    asyncio.run(init_models())
-    # uvicorn.run("app.main:app", host="127.0.0.1", port=8080, reload=True, reload_includes=["app.*"])
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8080, reload=True, reload_includes=["app.*"])
